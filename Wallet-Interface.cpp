65be9fba53b0517784307600fbef26d4563dc74e
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-11 22:50:40 +1000

    Added Implementation with Blank Functions

---
+++ Wallet-Interface.cpp
@@ -0,0 +1,18 @@
+#include "Wallet-Interface.h"
+
+
+std::string WalletInterface::login(std::string input) {
+
+}
+std::string WalletInterface::transaction(std::string input) {
+
+}
+std::string WalletInterface::smartContract(std::string input) {
+
+}
+std::string WalletInterface::transactionHistory(std::string input) {
+
+}
+std::string WalletInterface::logout() {
+    
+}
