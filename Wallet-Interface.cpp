f500ec1fa0042e17cd05af46747aa71195509670
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-11 23:00:51 +1000

    Added Run Implementation
    Added an implementation for the run function

--- Wallet-Interface.cpp
+++ Wallet-Interface.cpp
@@ -14,5 +14,18 @@ std::string WalletInterface::transactionHistory(std::string input) {
 
 }
 std::string WalletInterface::logout() {
-    
+
+}
+
+template<typename T>
+void CPPGUIExample::run(int port) {
+    // Create and fill the vectors with the custom functions
+    std::vector<std::string (T::*)(std::string)> functions;
+    functions.push_back(login);
+    functions.push_back(transaction);
+    functions.push_back(smartContract);
+    functions.push_back(transactionHistory);
+    functions.push_back(logout);
+
+    interface.run(port, functions);     // Run the C++ Interface
 }
